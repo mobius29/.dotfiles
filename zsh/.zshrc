c2ccf2cb70d075e11fe1cc633fe71079e81b578a
mobius29 <sagjin0000@naver.com>
2023-06-05 17:24:23 +0900

    feat: add develop.zsh file

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -5,4 +5,5 @@ source $DOTFILES_ZSH/alias.zsh
 source $DOTFILES_ZSH/git.zsh
 source $DOTFILES_ZSH/path.zsh
 source $DOTFILES_ZSH/plugin.zsh
+source $DOTFILES_ZSH/develop.zsh
 
---
+++ zsh/develop.zsh
@@ -0,0 +1,13 @@
+alias nrs="npm run start"
+alias nrd="npm run dev"
+alias nrb="npm run build"
+alias nrbd="npm run build:dev"
+alias nrdl="npm run dev:local"
+
+alias ya="yarn add"
+alias yaD="yarn add -D"
+alias yb="yarn build"
+alias ybd="yarn build:dev"
+alias yd="yarn dev"
+alias ys="yarn start"
+
