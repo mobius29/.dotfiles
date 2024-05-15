8f42e67030ec3561d18f2b4076d4ac7c26a76d44
mobius29 <sagjin0000@naver.com>
2024-05-15 14:05:09 +0900

    feat: plugins

---
+++ nvim/lua/plugins/LuaSnip.lua
@@ -0,0 +1,9 @@
+return {
+    -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+    {
+        "L3MON4D3/LuaSnip",
+        keys = function()
+            return {}
+        end,
+    },
+}
