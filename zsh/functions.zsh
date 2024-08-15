ec234593c5690af635c0b44f5da21471bd984746
mobius29 <sagjin0000@naver.com>
2024-08-15 23:16:51 +0900

    neofetch

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -10,6 +10,6 @@ searchInAllFiles() {
 
 findDirectory() {
   local dir
-  dir=$(find ${1:-~/Desktop} -path '*/\.*' -prune -maxdepth 4 -o -type d -print 2> /dev/null | fzf +m)
+  dir=$(find ${1:-~/Documents} -path '*/\.*' -prune -maxdepth 3 -o -type d -print 2> /dev/null | fzf +m)
   cd "$dir"
 }
