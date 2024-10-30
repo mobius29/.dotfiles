3d208cfcd1be3d6991b891952d5c3a4a8c604fa4
mobius29 <sagjin0000@naver.com>
2024-10-30 12:18:18 +0900

    update Brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # install brew
-if test ! $(which brew); then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if test ! "$(which brew)"; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # execute Brewfile
 brew update
 brew tap homebrew/bundle
-brew bundle --file=$HOME/.dotfiles/Brewfile
+brew bundle --file="$HOME"/.dotfiles/Brewfile
 brew cleanup
 brew cask cleanup
 
@@ -16,17 +16,5 @@ brew cask cleanup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # link .zshrc
-ln -nfs $HOME/.dotfiles/cfg/.zshrc $HOME/.zshrc
-source $HOME/.zshrc
-
-# # link .vimrc
-# sudo ln -nfs $HOME/.dotfiles/cfg/.vimrc
-# [ ! -d $HOME/.config || ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim
-# ln -nfs $HOME/.dotfiles/cfg/.vimrc $HOME/.config/nvim/init.vim
-
-# # install vimPlug
-# [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# # install plugin
-# vim +PlugInstall +qall
+ln -nfs "$HOME"/.dotfiles/cfg/.zshrc "$HOME"/.zshrc
+source "$HOME"/.zshrc
