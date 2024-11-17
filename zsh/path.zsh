6842df1498ac6c75754cfe704f32657464a7eba4
mobius29 <sagjin0000@naver.com>
2024-11-18 08:56:25 +0900

    rmv: remove nvm

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,12 +4,6 @@ eval $(/opt/homebrew/bin/brew shellenv)
 # nvim
 export EDITOR=/opt/homebrew/bin/nvim
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-
 # rust
 source "$HOME/.cargo/env"
 
