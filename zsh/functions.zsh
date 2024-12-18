39d153d56006d832fb2e7f90b2a7dbfd4c5bd6a1
mobius29 <sagjin0000@naver.com>
2024-12-19 08:53:42 +0900

    update

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,23 +3,3 @@ findDirectory() {
   dir=$(find ${1:-~/Documents} -path '*/\.*' -prune -maxdepth 3 -o -type d -print 2> /dev/null | fzf +m)
   cd "$dir"
 }
-
-jd() {
-  if ! $(command -v dasel &> /dev/null); then
-    echo "dasel is not installed. Please install dasel first"
-    echo "dasel can be installed by homebrew. 'brew install dasel'"
-
-    return 1
-  fi
-  
-  local pretty_json
-  pretty_json=$(dasel -r json $1)
-
-  echo $pretty_json
-}
-
-jd_test() {
-  source ~/.zshrc
-
-  echo '{ "name": { "first": "seongjin", "last": "lee" } }' | jd
-}
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -20,11 +20,22 @@ esac
 # pnpm end
 
 # fnm
+export PATH="$HOME/.local/state/fnm_multishells/14603_1731887886613/bin":$PATH
+export FNM_MULTISHELL_PATH="$HOME/.local/state/fnm_multishells/14603_1731887886613"
+export FNM_VERSION_FILE_STRATEGY="local"
+export FNM_DIR="$HOME/.local/share/fnm"
+export FNM_LOGLEVEL="info"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+export FNM_COREPACK_ENABLED="false"
+export FNM_RESOLVE_ENGINES="false"
+export FNM_ARCH="arm64"
+
 FNM_PATH="$HOME/Library/Application Support/fnm"
 if [ -d "$FNM_PATH" ]; then
   export PATH="/Users/sj_ly00/Library/Application Support/fnm:$PATH"
   eval "`fnm env`"
 fi
+
 
 # rust
 source "$HOME/.cargo/env"
