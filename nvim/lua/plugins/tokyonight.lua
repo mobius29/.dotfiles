a8804f8d640bcaa8b50475fcae4c050bec967448
mobius29 <sagjin0000@naver.com>
2025-02-26 11:54:13 +0900

    feat: update

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -6,17 +6,6 @@ return {
     ---@class tokyonight.Config
     opts = {
       priority = 999,
-      transparent = true,
-      terminal_colors = false,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-        comments = { fg = "#E6B9DE", italic = true },
-        keywords = { fg = "#CAEDFF", italic = true },
-        variables = { fg = "#F8E559", italic = true },
-        functions = { fg = "#8B93FF", italic = true },
-      },
-      on_colors = function(colors) end,
       cache = true,
     },
   },
