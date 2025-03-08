a5cfee0b8f48b4b9388ff4b6e205e8b9514a94cc
mobius29 <sagjin0000@naver.com>
2025-03-08 13:41:41 +0900

    feat: skipping each installation

--- bootstrap.py
+++ bootstrap.py
@@ -1,22 +1,18 @@
 import os
+import platform
+import subprocess
 import sys
 import shutil
-import subprocess
-import platform
 import argparse
 
-system = platform.system()
-
-if system == "Windows":
-    print("Windows is not supported.")
-    sys.exit(1)
-
-
-parser = argparse.ArgumentParser(description="Dotfiles setup script")
-parser.add_argument("--link-force", action="store_true", help="Force symlink creation")
-args = parser.parse_args()
-
+# ----------------------------- constants ------------------------------
 DOTFILES_ROOT = os.path.dirname(os.path.abspath(__file__))
+BREW_COMMANDS = [
+    ["update"],
+    ["tap", "homebrew/bundle"],
+    ["bundle", f"--file={DOTFILES_ROOT}/Brewfile"],
+    ["cleanup"],
+]
 SYMLINK_MAP = {
     "~/.zshrc": "configs/.zshrc",
     "~/.config/nvim": "nvim",
@@ -24,9 +20,10 @@ SYMLINK_MAP = {
     "~/.tmux.conf": "configs/.tmux.conf",
 }
 
-SYM_LINK_FORCE = args.link_force
+# --------------------------- constants end -------------------------------
 
 
+# ----------------------------- utils ----------------------------------
 def print_section(title: str):
     print(f"\n{'=' * 10} {title} {'=' * 10}\n")
 
@@ -49,60 +46,101 @@ def execute_command(command: str):
         sys.exit(1)
 
 
-def homebrew():
-    if shutil.which("brew") is None:
-        print_section("brew not Found. Installing Homebrew...")
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Dotfiles setup script")
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="Force the installation of a package or creation a symbolic link",
+    )
 
-        install_brew_cmd = '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-        execute_command(install_brew_cmd)
-        execute_command("brew doctor")
-
-    commands = [
-        ["update"],
-        ["tap", "homebrew/bundle"],
-        ["bundle", f"--file={DOTFILES_ROOT}/Brewfile"],
-        ["cleanup"],
-    ]
-    print_section("Installing Homebrew Packages")
-
-    for cmd in commands:
-        execute_command(" ".join(["brew", *cmd]))
+    return parser.parse_args()
 
 
-def symlink():
-    symlinked = []
+def continue_section(input_msg: str, skipped_msg: str):
+    is_continue = input(input_msg + " [y/N]: ")
+    while is_continue.lower() not in ["y", "n"]:
+        is_continue = input("Please enter 'y' or 'n': ")
 
-    for dest, src in SYMLINK_MAP.items():
-        src = os.path.join(DOTFILES_ROOT, src)
-        dest = os.path.expanduser(dest)
+    if is_continue.lower() == "n":
+        print(skipped_msg)
+        return False
 
-        if os.path.exists(dest):
-            if not SYM_LINK_FORCE:
-                print(f"{dest} already exists. Skipping.")
-                continue
-
-            print(f"{dest} already exists. Removing.")
-            os.remove(dest)
-
-        isSrcDir = os.path.isdir(src)
-        os.symlink(src, dest, target_is_directory=isSrcDir)
-        symlinked.append(f"{src} -> {dest}")
-
-    symlinked.sort()
-    print("\n".join(symlinked))
+    return True
 
 
-def oh_my_zsh():
-    omz_path = os.path.expanduser("~/.oh-my-zsh")
-    if os.path.exists(omz_path):
-        return
-
-    print_section("Installing Oh My Zsh")
-    command = 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-    execute_command(command)
+# --------------------------- utils end --------------------------------
 
 
 if __name__ == "__main__":
-    homebrew()
-    oh_my_zsh()
-    symlink()
+    system = platform.system()
+
+    # Check if the system is Windows
+    if system == "Windows":
+        print("Windows is not supported.")
+        sys.exit(1)
+
+    args = parse_arguments()
+
+    # Install Homebrew and packages
+    if continue_section(
+        "Do you want to install Homebrew and packages?",
+        "Skipping Homebrew installation",
+    ):
+        if shutil.which("brew") is None:
+            print_section("brew not Found. Installing Homebrew...")
+            execute_command(
+                '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+            )
+            execute_command("brew doctor")
+
+        print_section("Installing Homebrew Packages")
+        for cmd in BREW_COMMANDS:
+            execute_command(" ".join(["brew", *cmd]))
+
+    # Install Oh My Zsh
+    if continue_section(
+        "Do you want to install Oh My Zsh?",
+        "Skipping Oh My Zsh installation",
+    ):
+        omz_path = os.path.expanduser("~/.oh-my-zsh")
+        if os.path.exists(omz_path):
+            print("Oh My Zsh is already installed.")
+
+            if continue_section(
+                "Do you want to reinstall Oh My Zsh?",
+                "Skipping Oh My Zsh reinstallation",
+            ):
+                print_section("Installing Oh My Zsh")
+                execute_command(
+                    'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+                )
+
+    # Create symbolic links
+    if continue_section(
+        "Do you want to create symbolic links?",
+        "Skipping symbolic link creation",
+    ):
+        symlinked = []
+
+        for dest, src in SYMLINK_MAP.items():
+            src = os.path.join(DOTFILES_ROOT, src)
+            dest = os.path.expanduser(dest)
+
+            if os.path.exists(dest):
+                if not args.force:
+                    print(f"{dest} already exists. Skipping.")
+                    continue
+
+                print(f"{dest} already exists. Removing.")
+                os.remove(dest)
+
+            isSrcDir = os.path.isdir(src)
+            os.symlink(src, dest, target_is_directory=isSrcDir)
+            symlinked.append(f"{src} -> {dest}")
+
+        symlinked.sort()
+        print("\n".join(symlinked))
+
+    print_section("Done")
